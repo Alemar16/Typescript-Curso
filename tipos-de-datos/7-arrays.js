bc9f84296c79599ddfc1271349b021e2f63a3407
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-24 09:47:11 -0300

    array-ejemplos

---
+++ tipos-de-datos/7-arrays.js
@@ -0,0 +1,16 @@
+"use strict";
+//ARRAYS
+//ARREGLO DE NUMEROS
+const numeros = [1, 2, 3, 4, 5];
+//ARREGLO DE STRINGS
+const nombres = ["Juan", "Pedro", "Ana"];
+//ARREGLOS DE BOOLEANOS
+const booleanos = [true, false, true];
+//ARREGLO COMBINADOS
+//ARREGLO DE STRINGS Y NUMEROS
+const persona = ["Juan", 30];
+//ARREGLO DE OBJETOS
+const personas = [
+    { nombre: "Juan", edad: 30 },
+    { nombre: "Pedro", edad: 25 },
+];
---
+++ tipos-de-datos/7-arrays.ts
@@ -0,0 +1,24 @@
+//ARRAYS
+
+//ARREGLO DE NUMEROS
+const numeros: number[] = [1, 2, 3, 4, 5];
+
+//ARREGLO DE STRINGS
+const nombres: string[] = ["Juan", "Pedro", "Ana"];
+
+//ARREGLOS DE BOOLEANOS
+const booleanos: boolean[] = [true, false, true];
+
+
+
+
+//ARREGLO COMBINADOS
+
+//ARREGLO DE STRINGS Y NUMEROS
+const persona: (string | number)[] = ["Juan", 30];
+
+//ARREGLO DE OBJETOS
+const personas: { nombre: string; edad: number }[] = [
+  { nombre: "Juan", edad: 30 },
+  { nombre: "Pedro", edad: 25 },
+]; 
