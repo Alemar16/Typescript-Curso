75a8e7c44fb27f659c69c178be3958ea9ced22dd
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-24 11:08:35 -0300

    ejemplos de prueba 4

---
+++ 5-Arrays/prueba4.js
@@ -0,0 +1,7 @@
+"use strict";
+// Arreglos (ARRAY)
+let arreglo = [1, 2, 3, 4, 5, 6]; //DECLARACION DE UN ARREGLO de NUMEROS
+let arreglo2 = ['HTML', 'CSS', 'JAVASCRIPT']; //DECLARACION DE UN ARREGLO de STRINGS
+arreglo2[2].indexOf('HTML');
+console.log(arreglo2);
+//si el arreglo es de diferentes tipos de datos, se debe especificar el tipo de dato para cada elemento del arreglo combinando segun su tipo
---
+++ 5-Arrays/prueba4.ts
@@ -0,0 +1,10 @@
+// Arreglos (ARRAY)
+
+let arreglo: number[] = [1, 2, 3, 4, 5, 6]; //DECLARACION DE UN ARREGLO de NUMEROS
+
+let arreglo2: string[] = ['HTML', 'CSS', 'JAVASCRIPT']; //DECLARACION DE UN ARREGLO de STRINGS
+
+arreglo2[2].indexOf('HTML');
+console.log(arreglo2);
+
+//si el arreglo es de diferentes tipos de datos, se debe especificar el tipo de dato para cada elemento del arreglo combinando segun su tipo
