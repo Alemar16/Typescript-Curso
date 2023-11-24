5e3cedd55ce1444f76ed15bc8306967ab4255910
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-24 09:47:50 -0300

    clases-ejemplos

---
+++ tipos-de-datos/10-clases.js
@@ -0,0 +1,12 @@
+"use strict";
+//CLASES
+//DECLARACION DE UNA CLASE
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    saludar() {
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+}
---
+++ tipos-de-datos/10-clases.ts
@@ -0,0 +1,15 @@
+//CLASES
+
+//DECLARACION DE UNA CLASE
+class Persona {
+  nombre: string;
+  edad: number;
+  constructor(nombre: string, edad: number) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+  saludar() {
+    console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+  }
+}
+
