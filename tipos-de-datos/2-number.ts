b886fc788ddc11a1625fc4a45dc35bcfe8321e9a
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-23 23:40:55 -0300

    number-ejemplo

---
+++ tipos-de-datos/2-number.ts
@@ -0,0 +1,22 @@
+//NUMEROS
+
+//NUMEROS ENTEROS:
+const num1: number = 10;
+
+//NUMEROS DECIMALES:
+const num2: number = 0.2;//2 / 5 = 0.4
+
+//NOTACION EXPONENCIAL:
+const num3: number = 2.5e3;//2.5 * 10^3 = 2500
+
+//NOTACION EXPONENCIAL NEGATIVOS:
+const num4: number = 1.5e-2;//1.5 * 10^-2 = 0.015
+
+//HEXADECIMALES (BASE 16) UTILIZANDO EL PREFIJO 0x:
+const num5: number = 0xA;// VALOR DECIMAL:10
+
+//OCTALES (BASE 8) UTILIZANDO EL PREFIJO 0o:
+const num6: number = 0o12;// VALOR DECIMAL:10
+
+//BINARIOS (BASE 2) UTILIZANDO EL PREFIJO 0b:
+const num7: number = 0b1010;// VALOR DECIMAL:10
