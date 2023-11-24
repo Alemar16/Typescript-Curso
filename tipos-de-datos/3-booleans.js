6831c0d3e465718d1384a202524684f0a7affdb3
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-23 23:41:08 -0300

    boolean-ejemplo

---
+++ tipos-de-datos/3-booleans.js
@@ -0,0 +1,6 @@
+"use strict";
+//BOOLEANS
+//VALOR BOOLEANO TRUE
+const bool1 = true;
+//VALOR BOOLEANO FALSE
+const bool2 = false;
---
+++ tipos-de-datos/3-booleans.ts
@@ -0,0 +1,7 @@
+//BOOLEANS
+
+//VALOR BOOLEANO TRUE
+const bool1: boolean = true;
+
+//VALOR BOOLEANO FALSE
+const bool2: boolean = false;
