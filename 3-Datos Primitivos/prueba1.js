bb52da09611180176c8e565905fb0567f07760f0
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-24 10:29:43 -0300

    Ejemplo prueba 1

---
+++ 3-Datos Primitivos/prueba1.js
@@ -0,0 +1,9 @@
+"use strict";
+//EJEMPLO BASICO COM BOLEANOS
+let estudianteJavascript = false;
+if (estudianteJavascript) {
+    console.log('Puedes seguir viendo este curso');
+}
+else {
+    console.log('Primero tienes que ver el curso de JavaScript');
+}
---
+++ 3-Datos Primitivos/prueba1.ts
@@ -0,0 +1,12 @@
+
+//EJEMPLO BASICO COM BOLEANOS
+
+let estudianteJavascript: boolean = false;
+
+if (estudianteJavascript) {
+  console.log('Puedes seguir viendo este curso');
+} else {
+  console.log('Primero tienes que ver el curso de JavaScript');
+}
+
+
