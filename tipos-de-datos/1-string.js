0c97d296d778927cfeb6c383e0423e1ed1941275
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-23 23:40:41 -0300

    string-ejemplo

---
+++ tipos-de-datos/1-string.js
@@ -0,0 +1,10 @@
+"use strict";
+// STRING
+//COMILLAS DOBLES
+const str1 = "Hola";
+//COMILLAS SIMPLES
+const str2 = 'Hola';
+//COMILLAS INVERTIDAS (Template literal [backticks])
+const nombre = 'Juan';
+const edad = 30;
+const str3 = `Hola, me llamo ${nombre} y tengo ${edad} años.`;
---
+++ tipos-de-datos/1-string.ts
@@ -0,0 +1,13 @@
+// STRING
+
+//COMILLAS DOBLES
+const str1: string = "Hola";
+
+//COMILLAS SIMPLES
+const str2: string = 'Hola';
+
+//COMILLAS INVERTIDAS (Template literal [backticks])
+const nombre: string = 'Juan';
+const edad: number = 30;
+
+const str3: string = `Hola, me llamo ${nombre} y tengo ${edad} años.`
