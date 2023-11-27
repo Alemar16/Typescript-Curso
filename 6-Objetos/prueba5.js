81e1e6df398faf375284c72f994e10419e90af8b
Armando Martinez <alemar.martinez16@gmail.com>
2023-11-27 09:30:47 -0300

    Ejemplos de prueba5

---
+++ 6-Objetos/prueba5.js
@@ -0,0 +1,11 @@
+"use strict";
+// OBJETO
+//En TypeScript, es recomendable definir los tipos de los objetos siempre que sea posible. La principal ventaja de TypeScript es la verificación de tipos estáticos, que te ayuda a detectar errores en tiempo de compilación en lugar de en tiempo de ejecución. Definir los tipos para tus objetos permite aprovechar esta característica.
+let programador2 = {
+    nombre: 'Sergio',
+    edad: 25,
+    desarrollador: true,
+    tecnologias: ['html', 'css', 'js'],
+};
+console.log(programador2);
+//Aunque no es estrictamente necesario definir tipos para tus objetos, hacerlo mejora la robustez y la claridad de tu código al aprovechar las características de verificación de tipos de TypeScript.
